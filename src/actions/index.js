ac3d8d147c293f8a528bea3095fd78d792ca833e
bazingaedwaqrd <bazingaedward@gmail.com>
2017-11-28 11:26:12 +0800

    finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,12 @@ const UPLOAD_FILE = 'UPLOAD_FILE';
 const UPDATE_FILE = 'UPDATE_FILE';
 const DELETE_FILE = 'DELETE_FILE';
 const REFRESH_DIRECTORY = 'REFRESH_DIRECTORY';
+
+
+export const refreshDirectory = ( path ) => {
+  return {
+    type: REFRESH_DIRECTORY,
+    paths: paths,
+
+  }
+}
